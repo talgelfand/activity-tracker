2da0733c6b260a27bc895b706bbb0e7172c8fff8
Ilja Semencenko <iljariga4@inbox.lv>
2022-04-21 18:22:23 +0300

    toast is added for LogOut

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,17 +2,29 @@ import React from 'react'
 import { Nav, NavLink } from './Navbar.style'
 import { signOut } from 'firebase/auth'
 import { auth } from '../../firebase/config'
+import { toast } from 'react-toastify'
 
 const NavBar = () => {
   const currentUser = auth.currentUser
 
+  const toastConfig = {
+    position: 'bottom-center',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  }
+
   const logOut = () => {
+
     signOut(auth)
       .then(() => {
-        console.log('success logOut')
+        toast.success('You have logged out!', toastConfig)
       })
       .catch((error) => {
-        console.log(error)
+        toast.error('Sorry, an error occured', toastConfig)
+        console.error(error)
       })
   }
 
