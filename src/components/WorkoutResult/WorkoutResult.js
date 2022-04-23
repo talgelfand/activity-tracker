ece421de4228d66ed46169bed3a007dca8f5a557
Tal Gelfand <talj.gelfand@gmail.com>
2022-04-23 18:30:52 +0300

    create a new component to display info about workout

---
+++ src/components/WorkoutResult/WorkoutResult.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Card, ListGroup } from 'react-bootstrap'
+
+const WorkoutResult = ({ name, duration, calories }) => {
+  return (
+    <div className='d-flex justify-content-center mt-3'>
+      <Card style={{ width: '25rem' }}>
+        <ListGroup variant='flush'>
+          <ListGroup.Item className='d-flex'>
+            <p>
+              <strong>Type of workout:</strong> {name}
+            </p>
+          </ListGroup.Item>
+          <ListGroup.Item className='d-flex'>
+            <p>
+              <strong>Duration:</strong> {duration} minutes
+            </p>
+          </ListGroup.Item>
+          <ListGroup.Item className='d-flex'>
+            <p>
+              <strong>Calories burnt:</strong> {calories}
+            </p>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+    </div>
+  )
+}
+
+export default WorkoutResult
